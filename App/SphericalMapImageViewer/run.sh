83b1c310d5b54a6855c9af67f174b06425eac848
Naohisa Sakamoto <naohisa@SKMT-MacBookPro13TB.local>
2017-09-24 20:39:01 +0900

    Modified.

--- App/SphericalMapImageViewer/run.sh
+++ App/SphericalMapImageViewer/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 PROGRAM=${PWD##*/}
-FILENAME=~/Work/Data/4DStreatViewData/images/filename0000_0000.bmp
+#FILENAME=~/Work/Data/4DStreatViewData/images/filename0000_0000.bmp
+FILENAME=test.bmp
 ./$PROGRAM -f $FILENAME
