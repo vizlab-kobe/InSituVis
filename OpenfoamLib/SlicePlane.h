1ba7e3c3daac85908a10d4984cc8ba51bb29c3b7
yamaoka yoshiaki <yamaoka@y.local>
2020-01-30 13:45:17 +0900

    Add SlicePlane

---
+++ OpenfoamLib/SlicePlane.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+void SlicePlane( const std::vector<float> &values, int ncells, int nnodes, const std::vector<float> &vertex_coords, const std::vector<float> &cell_coords, const std::vector<int> &label, int time, float min_value, float max_value, const size_t repetitions, float isothr1, float isothr2, float isothr3);
+void CalculateMinMax( float& min_x, float& min_y, float& min_z, float& max_x, float& max_y, float & max_z );
+
