e801263687375f2359e35c09b785951ac1718553
Naohisa Sakamoto <naohisa.sakamoto@gmail.com>
2020-07-31 21:07:29 +0900

    Renamed.

---
+++ App/OralAirflow/rhoPimpleFoam_InSituVis/local/CreateUnstructuredVolumeObject.h
@@ -0,0 +1,192 @@
+#pragma once
+#include <vector>
+#include <algorithm>
+
+// OpenFOAM related headers
+#include "fvMesh.H"
+#include "volFields.H"
+
+// KVS related headers
+#include <kvs/UnstructuredVolumeObject>
+#include <kvs/ValueArray>
+#include <kvs/InverseDistanceWeighting>
+
+//#include "InverseDistanceWeighting.h"
+
+
+namespace local
+{
+
+namespace internal
+{
+
+inline kvs::ValueArray<kvs::Real32> CalculateCoords(
+    const Foam::fvMesh& mesh )
+{
+    const size_t nnodes = mesh.nPoints(); // number of nodes
+
+    kvs::ValueArray<float> coords( nnodes * 3 );
+    for ( size_t i = 0; i < nnodes; ++i )
+    {
+        coords[ 3 * i + 0 ] = mesh.points()[i].x();
+        coords[ 3 * i + 1 ] = mesh.points()[i].y();
+        coords[ 3 * i + 2 ] = mesh.points()[i].z();
+    }
+
+    return coords;
+}
+
+inline kvs::ValueArray<kvs::Real32> CalculateValues(
+    const Foam::fvMesh& mesh,
+    const Foam::volScalarField& field )
+{
+    const size_t ncells = mesh.nCells(); // number of cells
+    const size_t nnodes = mesh.nPoints(); // number of nodes
+
+    // Calculate values on each grid point.
+    kvs::InverseDistanceWeighting<kvs::Real32> idw( nnodes );
+    for ( size_t i = 0; i < ncells; ++i )
+    {
+        const auto& c = mesh.C()[i];
+        const kvs::Vec3 center( c.x(), c.y() , c.z() );
+        const float value = static_cast<float>( field[i] );
+        for ( auto& id : mesh.cellPoints()[i] )
+        {
+            const auto& p = mesh.points()[id];
+            const kvs::Vec3 vertex( p.x(), p.y(), p.z() );
+            const auto distance = ( center -vertex ).length();
+            idw.insert( id, value, distance );
+        }
+    }
+
+    return idw.serialize();
+}
+
+inline kvs::ValueArray<kvs::Real32> CalculateValues(
+    const Foam::fvMesh& mesh,
+    const Foam::volVectorField& field )
+{
+    const size_t ncells = mesh.nCells(); // number of cells
+    const size_t nnodes = mesh.nPoints(); // number of nodes
+
+    // Calculate values on each grid point.
+    kvs::InverseDistanceWeighting<kvs::Real32> idw( nnodes );
+    for ( size_t i = 0; i < ncells; ++i )
+    {
+        const auto& c = mesh.C()[i];
+        const kvs::Vec3 center( c.x(), c.y() , c.z() );
+        const float value = static_cast<float>( Foam::mag( field[i] ) );
+        for ( auto& id : mesh.cellPoints()[i] )
+        {
+            const auto& p = mesh.points()[id];
+            const kvs::Vec3 vertex( p.x(), p.y(), p.z() );
+            const auto distance = ( center -vertex ).length();
+            idw.insert( id, value, distance );
+        }
+    }
+
+    return idw.serialize();
+}
+
+inline kvs::ValueArray<kvs::UInt32> CalculateConnections(
+    const Foam::fvMesh& mesh )
+{
+    const size_t ncells = mesh.nCells(); // number of cells
+
+    std::vector<kvs::UInt32> connections;
+    for ( size_t i = 0; i < ncells; ++i )
+    {
+        // For only hex cells.
+        const auto cell_nnodes = mesh.cellPoints()[i].size();
+        const auto cell_nfaces = mesh.cells()[i].size();
+        if ( cell_nnodes == 8 && cell_nfaces == 6 )
+        {
+            kvs::UInt32 id[8];
+            std::copy_n( mesh.cellPoints()[i].begin(), 8, id );
+/*
+            connections.push_back( static_cast<kvs::UInt32>( id[4] ) );
+            connections.push_back( static_cast<kvs::UInt32>( id[5] ) );
+            connections.push_back( static_cast<kvs::UInt32>( id[6] ) );
+            connections.push_back( static_cast<kvs::UInt32>( id[7] ) );
+            connections.push_back( static_cast<kvs::UInt32>( id[0] ) );
+            connections.push_back( static_cast<kvs::UInt32>( id[1] ) );
+            connections.push_back( static_cast<kvs::UInt32>( id[2] ) );
+            connections.push_back( static_cast<kvs::UInt32>( id[3] ) );
+*/
+/*
+            connections.push_back( static_cast<kvs::UInt32>( id[3] ) );
+            connections.push_back( static_cast<kvs::UInt32>( id[7] ) );
+            connections.push_back( static_cast<kvs::UInt32>( id[6] ) );
+            connections.push_back( static_cast<kvs::UInt32>( id[2] ) );
+            connections.push_back( static_cast<kvs::UInt32>( id[0] ) );
+            connections.push_back( static_cast<kvs::UInt32>( id[4] ) );
+            connections.push_back( static_cast<kvs::UInt32>( id[5] ) );
+            connections.push_back( static_cast<kvs::UInt32>( id[1] ) );
+*/
+            connections.push_back( static_cast<kvs::UInt32>( id[4] ) );
+            connections.push_back( static_cast<kvs::UInt32>( id[6] ) );
+            connections.push_back( static_cast<kvs::UInt32>( id[7] ) );
+            connections.push_back( static_cast<kvs::UInt32>( id[5] ) );
+            connections.push_back( static_cast<kvs::UInt32>( id[0] ) );
+            connections.push_back( static_cast<kvs::UInt32>( id[2] ) );
+            connections.push_back( static_cast<kvs::UInt32>( id[3] ) );
+            connections.push_back( static_cast<kvs::UInt32>( id[1] ) );
+        }
+    }
+
+    return kvs::ValueArray<kvs::UInt32>( connections );
+}
+
+}
+
+inline kvs::UnstructuredVolumeObject* CreateUnstructuredVolumeObject(
+    const Foam::fvMesh& mesh,
+    const Foam::volScalarField& field )
+{
+    const auto coords = internal::CalculateCoords( mesh );
+    const auto values = internal::CalculateValues( mesh, field );
+    const auto connections = internal::CalculateConnections( mesh );
+
+    const auto nnodes = coords.size() / 3;
+    const auto ncells = connections.size() / 8;
+
+    auto* volume = new kvs::UnstructuredVolumeObject();
+    volume->setCellTypeToHexahedra();
+    volume->setVeclen( 1 );
+    volume->setNumberOfNodes( nnodes );
+    volume->setNumberOfCells( ncells );
+    volume->setValues( values );
+    volume->setCoords( coords );
+    volume->setConnections( connections );
+    volume->updateMinMaxValues();
+    volume->updateMinMaxCoords();
+
+    return volume;
+}
+
+inline kvs::UnstructuredVolumeObject* CreateUnstructuredVolumeObject(
+    const Foam::fvMesh& mesh,
+    const Foam::volVectorField& field )
+{
+    const auto coords = internal::CalculateCoords( mesh );
+    const auto values = internal::CalculateValues( mesh, field );
+    const auto connections = internal::CalculateConnections( mesh );
+
+    const auto nnodes = coords.size() / 3;
+    const auto ncells = connections.size() / 8;
+
+    auto* volume = new kvs::UnstructuredVolumeObject();
+    volume->setCellTypeToHexahedra();
+    volume->setVeclen( 1 );
+    volume->setNumberOfNodes( nnodes );
+    volume->setNumberOfCells( ncells );
+    volume->setValues( values );
+    volume->setCoords( coords );
+    volume->setConnections( connections );
+    volume->updateMinMaxValues();
+    volume->updateMinMaxCoords();
+
+    return volume;
+}
+
+} // end of namespace local
---
+++ App/OralAirflow/rhoPimpleFoam_InSituVis/rhoPimpleFoam.C
@@ -0,0 +1,226 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2013 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    rhoPimpleFoam
+
+Description
+    Transient solver for laminar or turbulent flow of compressible fluids
+    for HVAC and similar applications.
+
+    Uses the flexible PIMPLE (PISO-SIMPLE) solution for time-resolved and
+    pseudo-transient simulations.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "psiThermo.H"
+#include "turbulenceModel.H"
+#include "bound.H"
+#include "pimpleControl.H"
+#include "fvIOoptionList.H"
+
+// rhoPimpleFoam_PostHocVis: Headers
+// {
+#include <kvs/mpi/Communicator>
+#include <kvs/mpi/Logger>
+#include <kvs/Timer>
+#include <kvs/String>
+#include "local/CreateOutputDirectory.h"
+#include "local/CreateUnstructuredVolumeObject.h"
+// }
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+
+    pimpleControl pimple(mesh);
+
+    #include "createFields.H"
+    #include "createFvOptions.H"
+    #include "initContinuityErrs.H"
+
+    // rhoPimpleFoam_PostHocVis: Parameter settings
+    // {
+    Foam::messageStream::level = 0; // Disable Foam::Info
+    kvs::mpi::Communicator world( MPI_COMM_WORLD );
+    kvs::mpi::Logger logger( world );
+    kvs::Timer timer;
+    const kvs::Indent indent(4);
+    const int root = 0;
+    const int size = world.size();
+    const int rank = world.rank();
+    // }
+
+    // rhoPimpleFoam_PostHocVis: Create output directories
+    // {
+    const auto output_dirname = local::CreateOutputDirectory( world, "Output", "Proc" );
+    if ( output_dirname.empty() )
+    {
+        logger( root ) << "ERROR: " << "Cannot create output directory." << std::endl;
+        world.abort();
+    }
+    // }
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    // rhoPimpleFoam_PostHocVis: Output messages
+    // {
+    // Info<< "\nStarting time loop\n" << endl;
+    const auto start_time = runTime.startTime().value();
+    const auto start_time_index = runTime.startTimeIndex();
+    const auto end_time = runTime.endTime().value();
+    const auto end_time_index = static_cast<int>( end_time / runTime.deltaT().value() );
+    logger( root ) << std::endl;
+    logger( root ) << "STARTING TIME LOOP" << std::endl;
+    logger( root ) << indent << "Start time and index: " << start_time << ", " << start_time_index << std::endl;
+    logger( root ) << indent << "End time and index: " << end_time << ", " << end_time_index << std::endl;
+    logger( root ) << std::endl;
+    // }
+
+    while (runTime.run())
+    {
+        #include "readTimeControls.H"
+        #include "compressibleCourantNo.H"
+        #include "setDeltaT.H"
+
+        runTime++;
+
+        // rhoPimpleFoam_PostHocVis: Output messages
+        // {
+        // Info<< "Time = " << runTime.timeName() << nl << endl;
+        const auto current_time = runTime.timeName();
+        const auto current_time_index = runTime.timeIndex();
+        logger( root ) << "LOOP[" << current_time_index << "/" << end_time_index << "]: " << std::endl;
+        logger( root ) << indent << "T: " << current_time << std::endl;
+        logger( root ) << indent << "End T: " << end_time << std::endl;
+        logger( root ) << indent << "Delta T: " << runTime.deltaT().value() << std::endl;
+        // }
+
+        // rhoPimpleFoam_PostHocVis: Start timer
+        // {
+        timer.start();
+        // }
+
+        if (pimple.nCorrPIMPLE() <= 1)
+        {
+            #include "rhoEqn.H"
+        }
+
+        // --- Pressure-velocity PIMPLE corrector loop
+        while (pimple.loop())
+        {
+            #include "UEqn.H"
+            #include "EEqn.H"
+
+            // --- Pressure corrector loop
+            while (pimple.correct())
+            {
+                #include "pEqn.H"
+            }
+
+            if (pimple.turbCorr())
+            {
+                turbulence->correct();
+            }
+        }
+
+        runTime.write();
+
+        // rhoPimpleFoam_PostHocVis: Stop timer
+        // {
+        timer.stop();
+        // }
+
+        // rhoPimpleFoam_PostHocVis: Output messages
+        // {
+        const auto ts = timer.sec();
+        const auto Ts = kvs::String::ToString( ts, 4 );
+        logger( root ) << indent << "Processing Times:" << std::endl;
+        logger( root ) << indent.nextIndent() << "Simulation: " << Ts << " s" << std::endl;
+        //logger( root ) << indent.nextIndent().nextIndent() << "Number of nodes: " << mesh.nPoints() << std::endl;
+        //logger( root ) << indent.nextIndent().nextIndent() << "Number of cells: " << mesh.nCells() << std::endl;
+        // }
+
+        // rhoPimpleFoam_PostHocVis: Import mesh and field
+        // {
+        timer.start();
+        auto* volume = local::CreateUnstructuredVolumeObject( mesh, p ); // p: pressure
+        //auto* volume = local::CreateUnstructuredVolumeObject( mesh, U ); // U: velocity (magnitude)
+        timer.stop();
+        // }
+
+        // rhoPimpleFoam_PostHocVis: Output messages
+        // {
+        const auto ti = timer.sec();
+        const auto Ti = kvs::String::ToString( ti, 4 );
+        logger( root ) << indent.nextIndent() << "Import: " << Ti << " s" << std::endl;
+        //volume->print( logger( root ), indent.nextIndent().nextIndent() );
+        // }
+
+        // rhoPimpleFoam_PostHocVis: Output KVSML
+        timer.start();
+        const std::string output_basename("output");
+        const std::string output_filename = output_basename + "_" + current_time + ".kvsml";
+        volume->write( output_dirname + output_filename );
+        timer.stop();
+        delete volume;
+
+        // rhoPimpleFoam_PostHocVis: Output messages
+        // {
+        const auto to = timer.sec();
+        const auto To = kvs::String::ToString( to, 4 );
+        logger( root ) << indent.nextIndent() << "Write: " << To << " s" << std::endl;
+        // }
+
+        // rhoPimpleFoam_PostHocVis: Output messages
+        // {
+        const auto tt = ts + ti + to;
+        const auto Tt = kvs::String::ToString( tt, 4 );
+        logger( root ) << indent.nextIndent() << "---" << std::endl;
+        logger( root ) << indent.nextIndent() << "Total: " << Tt << " s" << std::endl;
+        // }
+
+        // rhoPimpleFoam_PostHocVis: Output messages
+        // {
+        // Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+        //     << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+        //     << nl << endl;
+        const auto elapsed_time = runTime.elapsedCpuTime();
+        logger( root ) << std::endl;
+        logger( root ) << "Elapsed Time: " << elapsed_time << " s" << std::endl;
+        logger( root ) << std::endl;
+        // }
+    }
+
+    //Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
