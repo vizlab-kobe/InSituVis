f4f9dcbc69de67faa8ed5d1b2d69478380e43cb7
Naohisa Sakamoto <naohisa.sakamoto@gmail.com>
2020-08-28 18:24:17 +0900

    Modified.

---
+++ App/OralAirflow/realistic-s1/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+PROGRAM=rhoPimpleFoam_InSituVis
+
+if [ -z $WM_DIR ]; then
+    . ../etc/bashrc
+fi
+
+mpirun -n 4 ../$PROGRAM/$PROGRAM -parallel
+
+if type "ffmpeg" > /dev/null 2>&1; then
+    ffmpeg -r 60 -start_number 00001 -i Output/output_%05d.png -vcodec libx264 -pix_fmt yuv420p -r 60 output.mp4
+fi
