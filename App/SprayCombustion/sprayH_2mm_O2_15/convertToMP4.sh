15bd9d37b0713517cd97955def88664f4a27aee2
Taiki Sako <tsako@s168.lab>
2020-12-03 17:39:37 +0900

    add convertToMP4

---
+++ App/SprayCombustion/sprayH_2mm_O2_15/convertToMP4.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+OUTPUT_DIR="Output"
+for i in `seq 0 26`
+do
+ index=$(printf "%06d" $i)
+ ffmpeg -r 60 -start_number 000001 -i ${OUTPUT_DIR}/output_%06d_${index}.bmp -vcodec libx264 -pix_fmt yuv420p -r 60 output_${index}.mp4
+done
