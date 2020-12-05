61d6090b624552392e8f8040e331dd16d3ea9ab4
Taiki Sako <tsako@s168.lab>
2020-12-06 02:56:19 +0900

    Fixed convert scripts

--- App/OralAirflow/realistic-s3/convertToMP4.sh
+++ App/OralAirflow/realistic-s3/convertToMP4.sh
@@ -3,5 +3,5 @@ OUTPUT_DIR="Output/Process0000"
 for i in `seq 0 26`
 do
  index=$(printf "%06d" $i)
- ffmpeg -r 60 -start_number 000001 -i ${OUTPUT_DIR}/output_%06d_${index}.bmp -vcodec libx264 -pix_fmt yuv420p -r 60 output_${index}.mp4
+ ffmpeg -framerate 60 -pattern_type glob -i "${OUTPUT_DIR}/output_*_${index}.bmp" -vcodec libx264 -pix_fmt yuv420p -r 60 output_${index}.mp4
 done
