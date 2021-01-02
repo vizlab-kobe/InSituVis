9c2f8ceb82bfd5d100e73ba1802cc1b8e66f9e25
Naohisa Sakamoto <naohisa.sakamoto@people.kobe-u.ac.jp>
2021-01-02 14:45:55 +0900

    Update Adaptor.h

--- Lib/Adaptor.h
+++ Lib/Adaptor.h
@@ -213,7 +213,7 @@ protected:
         return buffer;
     }
 
-    bool isInsideVolume( const kvs::Vec3& position ) const
+    bool isInsideVolume( const kvs::Vec3& position )
     {
         const auto* object = m_screen.scene()->objectManager();
         const auto min_obj = object->minObjectCoord();
