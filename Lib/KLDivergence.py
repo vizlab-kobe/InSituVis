d48f631d4f43e9679cb14501b10a329eb14a241a
Naohisa Sakamoto <naohisa.sakamoto@gmail.com>
2021-06-23 21:26:43 +0900

    Update KLDivergence.py

--- Lib/KLDivergence.py
+++ Lib/KLDivergence.py
@@ -5,8 +5,8 @@ def main( V_crr , V_prv ):
     KDE_crr = gaussian_kde( np.array( V_crr ) )
     KDE_prv = gaussian_kde( np.array( V_prv ) )
 
-    P_crr = KDE_crr( np.linspace(3, 45) )
-    P_prv = KDE_prv( np.linspace(3, 45) )
+    P_crr = KDE_crr( np.linspace( 3, 45 ) )
+    P_prv = KDE_prv( np.linspace( 3, 45 ) )
 
     D = entropy( P_crr, P_prv )
 
