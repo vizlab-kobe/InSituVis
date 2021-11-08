8baf09cc14f0db51206d6e925efb2d2760335dd4
KenIwata <futen05410@docomo.ne.jp>
2021-11-08 18:14:45 +0900

    Modified

--- Test/Entropy/main.cpp
+++ Test/Entropy/main.cpp
@@ -3,8 +3,9 @@
 #include <iomanip>
 #include <kvs/StructuredVolumeObject>
 #include <kvs/StructuredVolumeImporter>
+#include <kvs/PolygonObject>
+#include <kvs/Isosurface>
 #include <kvs/HydrogenVolumeData>
-#include <kvs/RayCastingRenderer>
 #include <kvs/Timer>
 #include <kvs/OffScreen>
 
@@ -13,13 +14,19 @@ int main( int argc, char** argv )
 {
     std::cout << "OSMesa version: " << kvs::osmesa::Version() << std::endl;
 
-    auto* object = new kvs::HydrogenVolumeData( { 64, 64, 64 } );
-    auto* renderer = new kvs::glsl::RayCastingRenderer();
-    //auto* renderer = new kvs::RayCastingRenderer();
+    auto* volume = new kvs::HydrogenVolumeData( { 64, 64, 64 } );
+
+    // Extract isosurfaces as polygon object.
+    const auto i = ( volume->maxValue() + volume->minValue() ) * 0.5; // isolevel
+    const auto n = kvs::PolygonObject::VertexNormal; // normal type
+    const auto d = false; // false: duplicated vertices will be removed
+    const auto t = kvs::TransferFunction( 256 ); // transfer function
+    auto* object = new kvs::Isosurface( volume, i, n, d, t );
+    delete volume;
 
     kvs::OffScreen screen;
     screen.setSize( 512, 512 );
-    screen.registerObject( object, renderer );
+    screen.registerObject( object );
 
     kvs::Timer timer( kvs::Timer::Start );
     for ( size_t i = 0; i < 12; i++ )
